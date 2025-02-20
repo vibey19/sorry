e3dc9aad69b087902ed83661007f1cbd030ecc00
Jainil Dadhaniya <130295809+vibey19@users.noreply.github.com>
2025-02-21 02:32:50 +0530

    Update yes.js

--- Pages/Page 5/yes.js
+++ Pages/Page 5/yes.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+
+// Create 100 hearts
 for (var i = 1; i <= 100; i++) {
   const hearts = document.createElement("div");
   hearts.classList.add("heart");
@@ -9,18 +11,18 @@ function animateHearts() {
   anime({
     targets: ".heart",
     translateX: function () {
-      return anime.random(-700, 700);
+      return anime.random(-window.innerWidth / 2, window.innerWidth / 2);
     },
     translateY: function () {
-      return anime.random(-500, 500);
+      return anime.random(-window.innerHeight / 2, window.innerHeight / 2);
     },
     rotate: 45,
     scale: function () {
-      return anime.random(1, 5);
+      return anime.random(0.5, 3);
     },
     easing: "easeInOutBack",
-    duration: 1000,
-    delay: anime.stagger(10),
+    duration: 2000,
+    delay: anime.stagger(20),
     complete: animateHearts,
   });
 }
